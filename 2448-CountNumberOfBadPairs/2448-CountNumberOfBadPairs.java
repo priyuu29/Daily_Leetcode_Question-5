04a317b607f2baad403cffc7391009e375bdb0c7
priyuu29 <142709652+priyuu29@users.noreply.github.com>
2025-08-13 10:23:29 +0530

    Create 2448-CountNumberOfBadPairs

---
+++ 2448-CountNumberOfBadPairs/2448-CountNumberOfBadPairs.java
@@ -0,0 +1,16 @@
+// Last updated: 8/13/2025, 10:23:27 AM
+import java.util.HashMap;
+import java.util.Map;
+class Solution {
+    public long countBadPairs(int[] nums) {
+        int n = nums.length;
+        long totalPairs = (long)n * (n - 1) / 2;
+        Map<Integer, Long> diffCount = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            int diff = nums[i] - i;
+            totalPairs -= diffCount.getOrDefault(diff, 0L);
+            diffCount.put(diff, diffCount.getOrDefault(diff, 0L) + 1);
+        }
+        return totalPairs;
+    }
+}
