7ece2602f1342b436050498d1d2eeea63f489923
priyuu29 <142709652+priyuu29@users.noreply.github.com>
2025-08-13 10:24:31 +0530

    Create 1524-StringMatchingInAnArray

---
+++ 1524-StringMatchingInAnArray/1524-StringMatchingInAnArray.java
@@ -0,0 +1,20 @@
+// Last updated: 8/13/2025, 10:24:29 AM
+import java.util.*;
+
+class Solution {
+    public List<String> stringMatching(String[] words) {
+        int n = words.length;
+        List<String> ans = new ArrayList<>();
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i != j && words[j].contains(words[i])) {
+                    ans.add(words[i]);
+                    break;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
