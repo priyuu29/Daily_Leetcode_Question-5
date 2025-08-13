5741865e6ced3cebf787923d69ccc7d119eb810c
priyuu29 <142709652+priyuu29@users.noreply.github.com>
2025-08-13 10:23:06 +0530

    Create 2792-NeighboringBitwiseXor

---
+++ 2792-NeighboringBitwiseXor/2792-NeighboringBitwiseXor.java
@@ -0,0 +1,18 @@
+// Last updated: 8/13/2025, 10:23:04 AM
+class Solution {
+    public boolean doesValidArrayExist(int[] derived) {
+        int n=derived.length;
+        int[] org=new int[n];
+
+        for(int i=1; i<n; i++){
+            org[i]=derived[i-1]^org[i-1];
+        }
+        
+        for(int i=0; i<n; i++){
+            int idx=(i+1)%n;
+            if(derived[i]!=(org[i]^org[idx])) return false;
+        }
+
+        return true;
+    }
+}
