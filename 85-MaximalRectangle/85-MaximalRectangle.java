751986cb3873d68a00fa3b20ed5d1b7ffd034259
priyuu29 <142709652+priyuu29@users.noreply.github.com>
2025-08-13 10:26:50 +0530

    Create 85-MaximalRectangle

---
+++ 85-MaximalRectangle/85-MaximalRectangle.java
@@ -0,0 +1,49 @@
+// Last updated: 8/13/2025, 10:26:48 AM
+class Solution {
+    public int maximalRectangle(char[][] matrix) {
+        int ans=0;
+        int [] arr=new int [matrix[0].length];
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(matrix[i][j]=='0'){
+                    arr[j]=0;
+                }else{
+                    arr[j]++;
+                }
+            }
+            ans=Math.max(ans,largest_Histogram(arr));
+        }
+        return ans;
+    }
+    public static int largest_Histogram(int [] arr) {
+		Stack<Integer> st=new Stack<>();
+		int ans=0;
+		for(int i=0;i<arr.length;i++) {
+			while(!st.isEmpty()&&arr[i]<arr[st.peek()]) {				
+				int h=arr[st.pop()];
+				int r=i;
+				if(st.isEmpty()) {
+					ans=Math.max(h*r, ans);
+				}else {
+					int l=st.peek();
+					int area=h*(r-l-1);
+					ans=Math.max(area, ans);
+				}
+			}
+			st.push(i);
+		}
+		int r=arr.length;
+		while(!st.isEmpty()) {				
+			int h=arr[st.pop()];
+//			int r=i;
+			if(st.isEmpty()) {
+				ans=Math.max(h*r, ans);
+			}else {
+				int l=st.peek();
+				int area=h*(r-l-1);
+				ans=Math.max(area, ans);
+			}
+		}
+		return ans;
+	}
+}
