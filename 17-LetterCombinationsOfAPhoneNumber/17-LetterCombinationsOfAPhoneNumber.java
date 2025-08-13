3f639fcd4f58ffde45dabf1c76dead938cd5ffb5
priyuu29 <142709652+priyuu29@users.noreply.github.com>
2025-08-13 10:27:22 +0530

    Create 17-LetterCombinationsOfAPhoneNumber

---
+++ 17-LetterCombinationsOfAPhoneNumber/17-LetterCombinationsOfAPhoneNumber.java
@@ -0,0 +1,25 @@
+// Last updated: 8/13/2025, 10:27:20 AM
+class Solution {
+    static String[] key = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs","tuv","wxyz"};
+    public List<String> letterCombinations(String digits) {
+        List<String>ll=new ArrayList<>();
+         if(digits.length()==0){
+            return ll;
+        }
+        Print(digits,"",ll);
+        return ll;
+    }
+    public static void Print(String ques ,String ans,List<String>ll) {
+       
+		if(ques.length()==0) {
+			ll.add(ans);
+			return;
+		}
+		
+		char ch=ques.charAt(0);
+		String getString=key[ch-'0'];
+		for(int i=0;i<getString.length();i++) {
+			Print(ques.substring(1),ans+getString.charAt(i),ll);
+		}
+	}
+}
