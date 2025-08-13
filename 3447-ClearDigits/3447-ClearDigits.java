a1da8449df2601ddf704cd466a6a8edb8d31cccf
priyuu29 <142709652+priyuu29@users.noreply.github.com>
2025-08-13 10:22:41 +0530

    Create 3447-ClearDigits

---
+++ 3447-ClearDigits/3447-ClearDigits.java
@@ -0,0 +1,16 @@
+// Last updated: 8/13/2025, 10:22:40 AM
+class Solution {
+    public String clearDigits(String s) {
+        StringBuilder stack = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (Character.isDigit(c)) {
+                if (stack.length() > 0) {
+                    stack.deleteCharAt(stack.length() - 1);
+                }
+            } else {
+                stack.append(c);
+            }
+        }
+        return stack.toString();
+    }
+}
