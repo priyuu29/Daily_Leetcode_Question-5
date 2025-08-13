a5a0ca2ccc9d1f1dcdbc99f04f2c281ab4bd0197
priyuu29 <142709652+priyuu29@users.noreply.github.com>
2025-08-13 10:26:11 +0530

    Create 236-LowestCommonAncestorOfABinaryTree

---
+++ 236-LowestCommonAncestorOfABinaryTree/236-LowestCommonAncestorOfABinaryTree.java
@@ -0,0 +1,30 @@
+// Last updated: 8/13/2025, 10:26:10 AM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root==null){
+            return null;
+        }
+        if(root==p || root==q){
+            return root;
+        }
+        TreeNode left=lowestCommonAncestor(root.left,p,q);
+        TreeNode right=lowestCommonAncestor(root.right,p,q);
+        if(left!=null && right!=null){
+            return root;
+        }
+        if (left==null){
+            return right;
+        }
+        return left;
+
+    }
+}
