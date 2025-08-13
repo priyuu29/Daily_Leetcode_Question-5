d5c74aa90e790357f628d5a73baa66f2f5c741cc
priyuu29 <142709652+priyuu29@users.noreply.github.com>
2025-08-13 10:24:54 +0530

    Create 1160-LetterTilePossibilities

---
+++ 1160-LetterTilePossibilities/1160-LetterTilePossibilities.java
@@ -0,0 +1,24 @@
+// Last updated: 8/13/2025, 10:24:52 AM
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    Set<String> all = new HashSet<>();
+
+    public int numTilePossibilities(String tiles) {
+        boolean[] visited = new boolean[tiles.length()];
+        backtrack(tiles, "", visited);
+        return all.size();
+    }
+
+    private void backtrack(String tiles, String word, boolean[] visited) {
+        for (int i = 0; i < tiles.length(); i++) {
+            if (visited[i]) continue;
+            String charSequence = word + tiles.charAt(i);
+            visited[i] = true;
+            all.add(charSequence);
+            backtrack(tiles, charSequence, visited);
+            visited[i] = false;
+        }
+    }
+}
