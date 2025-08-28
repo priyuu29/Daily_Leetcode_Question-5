593d50adee6aaece43f15c33d84f8ff49c979760
priyuu29 <142709652+priyuu29@users.noreply.github.com>
2025-08-28 12:18:07 +0530

    Create 617-MergeTwoBinaryTrees

---
+++ 617-MergeTwoBinaryTrees/617-MergeTwoBinaryTrees.java
@@ -0,0 +1,30 @@
+// Last updated: 8/28/2025, 12:18:07 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        return merge(root1,root2);
+    }
+    private TreeNode merge(TreeNode root1,TreeNode root2){
+      if(root1==null && root2==null) return null;
+      if(root1==null) return root2;
+      if(root2==null) return root1;
+      root1.val+=root2.val;
+      root1.left=merge(root1.left,root2.left);
+      root1.right=merge(root1.right,root2.right);
+      return root1;
+    }
+}
