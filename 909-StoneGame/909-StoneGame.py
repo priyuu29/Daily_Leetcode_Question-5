8170d2861d1c7b19547fd1354a62f2c3ec9b9b36
priyuu29 <142709652+priyuu29@users.noreply.github.com>
2025-09-03 12:11:53 +0530

    Create 909-StoneGame

---
+++ 909-StoneGame/909-StoneGame.py
@@ -0,0 +1,9 @@
+# Last updated: 9/3/2025, 12:11:52 PM
+class Solution(object):
+    def stoneGame(self, piles):
+        """
+        :type piles: List[int]
+        :rtype: bool
+        """
+        return True
+        
