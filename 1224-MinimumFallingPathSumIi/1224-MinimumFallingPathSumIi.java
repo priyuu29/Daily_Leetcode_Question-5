2bd2c6905ac4923d170d6039ddd858b6615e3bb9
priyuu29 <142709652+priyuu29@users.noreply.github.com>
2025-09-28 08:26:53 +0530

    Create 1224-MinimumFallingPathSumIi

---
+++ 1224-MinimumFallingPathSumIi/1224-MinimumFallingPathSumIi.java
@@ -0,0 +1,28 @@
+// Last updated: 9/28/2025, 8:26:52 AM
+class Solution {
+    public int minFallingPathSum(int[][] matrix) {
+        int [][]dp=new int[matrix.length][matrix[0].length];
+        for(int[] i:dp){
+            Arrays.fill(i,-1000000);
+        }
+        int ans=Integer.MAX_VALUE;
+        for(int i=0;i<matrix[0].length;i++){
+            ans=Math.min(ans,min(matrix,0,i,dp));
+        }
+        return ans;
+    }
+    public int min(int [][] arr,int cr,int cc,int [][]dp){
+        if(cr==arr.length-1){
+            return arr[cr][cc];
+        }
+        if(dp[cr][cc]!=-1000000) return dp[cr][cc];
+        // int ld=min(arr,cr+1,cc-1,dp);
+        int ans=Integer.MAX_VALUE;
+
+        for(int i=0;i<arr[0].length;i++){
+            if(i==cc) continue;
+            ans=Math.min(ans,min(arr,cr+1,i,dp));
+        }
+        return dp[cr][cc]=ans+arr[cr][cc];
+    }
+}
